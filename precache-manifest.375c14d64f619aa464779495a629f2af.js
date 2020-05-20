42a5828c9d61b1eea56e33ff3c975f1d81810579
ljeabmreosn <brewcrew5498@outlook.com>
2020-05-20 22:15:27 +0000

    Deploying to gh-pages from  @ c73ab43e1878c4ebf1ce1191cb6844e476bc74db 🚀

---
+++ precache-manifest.375c14d64f619aa464779495a629f2af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d41825d648c0468ef5b1ebb67ee565",
+    "url": "/quasi-key-tableaux/index.html"
+  },
+  {
+    "revision": "22ad83856b3e113dff74",
+    "url": "/quasi-key-tableaux/static/css/2.82dca9e4.chunk.css"
+  },
+  {
+    "revision": "af968b2e0c2f6ddcae53",
+    "url": "/quasi-key-tableaux/static/css/main.e1be1cae.chunk.css"
+  },
+  {
+    "revision": "22ad83856b3e113dff74",
+    "url": "/quasi-key-tableaux/static/js/2.3afcb79a.chunk.js"
+  },
+  {
+    "revision": "af968b2e0c2f6ddcae53",
+    "url": "/quasi-key-tableaux/static/js/main.f3007aa2.chunk.js"
+  },
+  {
+    "revision": "7f577dbf3ac75ba14c92",
+    "url": "/quasi-key-tableaux/static/js/runtime-main.d02716bf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quasi-key-tableaux/precache-manifest.553a1f9d35d47b9b0b0429f6c796cfc7.js"
+  "/quasi-key-tableaux/precache-manifest.375c14d64f619aa464779495a629f2af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quasi-key-tableaux/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
