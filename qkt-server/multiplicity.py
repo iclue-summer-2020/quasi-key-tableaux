5d4e2d6b5424af676472caa1ff9f314e5368e914
= <brewcrew5498@outlook.com>
2020-06-02 17:10:55 -0500

    Use reduced-form patterns instead.

--- qkt-server/multiplicity.py
+++ qkt-server/multiplicity.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 from QK import QKTableaux, consoleFn
 
 KM = [(0, 1, 2), (0, 0, 2, 2), (0, 0, 2, 1), (1, 0, 3, 2), (1, 0, 2, 2)]
-KMP = [(0, 2, 4), (1, 0, 4, 3), (1, 2, 5, 4), (2, 1, 4, 5), (1, 2, 5, 6)]
+KMP = [(0, 2, 4), (1, 0, 4, 3), (0, 1, 4, 3), (1, 0, 3, 4), (0, 1, 4, 5)]
 
 def compositions(k, max_width, unique=False):
   '''
